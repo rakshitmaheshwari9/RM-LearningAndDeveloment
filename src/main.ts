d8f12fe73874e2eeb3ad431c18faa7fd87e1fbb9
Rakshit Maheshwari <rakshit.maheshwari@zonoapp.com>
2023-09-26 10:19:38 +0530

    First commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { createConnection } from 'typeorm';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await createConnection()
+  await app.listen(3000);
+}
+bootstrap();
